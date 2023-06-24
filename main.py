5cefb1e1f98c5ff39f40fb4154a46592665b9ebd
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-24 11:05:19 +0900

    number

--- main.py
+++ main.py
@@ -6,13 +6,24 @@ import asyncio
 parser = argparse.ArgumentParser(description='サジェスト汚染ツール')
 
 parser.add_argument("words", nargs="*", type=str, help="ターゲットのワード")
+parser.add_argument("-n", "--number", default=0, type=int, help="実行回数。0にすると無限回。")
 
 args = parser.parse_args()
+
 
 async def main():
     # System
     suggest = Suggest(args.words)
+    
+    index = 0
+    while True:
+        print(index)
+        await suggest.pollution()
+        if args.number != 0:
+            # 有限
+            index += 1
+            if index == args.number:
+                break
 
-    await suggest.pollution()
 
 asyncio.run(main())
