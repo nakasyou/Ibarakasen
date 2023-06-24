5d840d466032cb8bf7e7c1f3d3b6d1dd17ad99b2
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-24 10:42:42 +0900

    fix import

--- engines/google.py
+++ engines/google.py
@@ -1,4 +1,4 @@
-from ..utils.request_from_async import request_from_async
+from utils.request_from_async import request_from_async
 
 async def google(keyword: str):
     await request_from_async("https://www.google.com/search", {
