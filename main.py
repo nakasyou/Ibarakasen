843621252c58ca83e14d8b5c3d63285fc6b6148a
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-24 11:12:48 +0900

    tqdm

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from core.suggest import Suggest
 import argparse
 import asyncio
+from tqdm import tqdm
 
 # ArgParse
 parser = argparse.ArgumentParser(description='サジェスト汚染ツール')
@@ -14,14 +15,17 @@ args = parser.parse_args()
 async def main():
     # System
     suggest = Suggest(args.words)
+
+    if args.number != 0:
+        counter = tqdm(range(args.number))
     
-    index = 0
     while True:
         print(index)
         await suggest.pollution()
         if args.number != 0:
             # 有限
             index += 1
+            next(counter)
             if index == args.number:
                 break
 
